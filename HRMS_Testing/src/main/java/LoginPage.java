23005f5a4c34b57ba13684ef4f101d3f4de3453e
Aley Amin <aley.amin.22@gmail.com>
2024-07-27 22:28:01 +0300

    Added some logic to the login page | Added comments for communication

--- HRMS_Testing/src/main/java/LoginPage.java
+++ HRMS_Testing/src/main/java/LoginPage.java
@@ -8,6 +8,8 @@ public class LoginPage extends JFrame implements ActionListener {
     private JPasswordField passwordField;
     private JButton submit;
 
+    // private int loginStatus;
+
     public LoginPage() {
         setVisible(true);
 
@@ -22,17 +24,29 @@ public class LoginPage extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        LoginPage loginPage = new LoginPage();
+        new LoginPage();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == submit) {
-            String username = usernameField.getText();
-            String password = new String(passwordField.getPassword());
 
-            System.out.println("Username: " + username);
-            System.out.println("Password: " + password);
+            // .trim() to remove unwanted spaces --- zay el adv soft
+
+            String username = usernameField.getText().trim();
+            String password = new String(passwordField.getPassword()).trim();
+
+            // loginStatus = ----.authenicate(username,password);
+
+            // replace this part with the authenticate method when completed
+            // for example authenicate returns -1 if invalid, 1 if user, 2 if admin
+
+            if (username.equals("admin") && password.equals("admin")) {
+                JOptionPane.showMessageDialog(this, "Welcome Admin" );
+            }
+            else if (username.isEmpty() && password.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 }
