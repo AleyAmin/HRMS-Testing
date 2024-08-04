03bfda7678b49606edb77cc7a45393bff4d5cb5e
Hassan <hassanelmarsafy@gmail.com>
2024-08-04 15:46:04 +0300

    HRemployee class and incomplete GUI for employee page

---
+++ HRMS_Testing/src/main/java/classes/EmployeePage.java
@@ -0,0 +1,63 @@
+package classes;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class EmployeePage  extends JFrame implements ActionListener, KeyListener {
+    private JPanel EmployeePanel;
+    private JButton submit;
+    private JButton requestLeaveButton;
+    private JButton viewEvaluationButton;
+
+    public EmployeePage() {
+        setVisible(true);
+
+        setContentPane(EmployeePanel);
+        setTitle("HRMS");
+        setSize(500,500);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        submit.addActionListener(this);
+        requestLeaveButton.addActionListener(this);
+        viewEvaluationButton.addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submit) {
+        }
+        else if (e.getSource() == requestLeaveButton) {
+        }
+        else if (e.getSource() == viewEvaluationButton) {
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(EmployeePage::createAndShowGUI);
+    }
+
+    private static void createAndShowGUI() {new EmployeePage();}
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+
+}
